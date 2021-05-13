86a6792f4f4ada4e4e6e9f6a688ac4d11380f401
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-13 02:42:19 -0400

    nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :lists
-      resources :books
+      resources :lists do
+        resources :books
+      end
     end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
