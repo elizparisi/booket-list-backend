1f43819d4020cb5137ee5761492057ebebeb21db
Elizabeth Parisi <parisielizabeth@gmail.com>
2021-05-13 20:33:11 -0400

    add create action to lists controller

--- app/controllers/api/v1/lists_controller.rb
+++ app/controllers/api/v1/lists_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::ListsController < ApplicationController
 
-  before_action :set_list
+  # before_action :set_list
 
   def index
     lists = List.all
@@ -13,7 +13,13 @@ class Api::V1::ListsController < ApplicationController
   end
 
   def create
+    list = List.new(list_params)
 
+    if list.save
+      render json: ListSerializer.new(list), status: accepted
+    else
+      render json: { errors: list.errors.full_messages }, status: unprocessible_entity
+    end
   end
 
   def destroy
@@ -22,9 +28,9 @@ class Api::V1::ListsController < ApplicationController
 
   private
 
-  def set_list
-    @list = List.find(params[:list_id])
-  end
+  # def set_list
+  #   @list = List.find(params[:list_id])
+  # end
 
   def list_params
     params.require(:list).permit(:name)
